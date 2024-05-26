1f1be96917e964b3de97c69a3d17dbcd8a9de0b3
Dasha-Morozova <56588397+Dasha-Morozova@users.noreply.github.com>
2024-05-26 21:11:36 +0300

    Create 11.py
    Начальное

---
+++ 11.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = [
+    [11, 12, 13],
+    [21, 2, 20],
+    [1, 44, 15]
+    ]
+
+data = pd.DataFrame(df)
+Selected_features=[0,1,2]
+# Просмотр корреляции
+def CorrPlt(data,Selected_features):
+  X = data[Selected_features]
+  plt.subplots(figsize=(8, 5))
+  sns.heatmap(X.corr(), annot=True, cmap="RdYlGn",vmin=-1, vmax=1)
+  plt.show()
+print(CorrPlt(data,Selected_features))
